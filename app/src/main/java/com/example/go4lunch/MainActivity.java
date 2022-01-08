f1cf4006a93b7ec2cfbbac18ba001d8d5dcb2cfd
JDR <jerome.diaz-rey@coppernic.fr>
2022-01-08 17:59:15 +0100

    set the facebook button and google on the xml

--- app/src/main/java/com/example/go4lunch/MainActivity.java
+++ app/src/main/java/com/example/go4lunch/MainActivity.java
@@ -4,11 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.go4lunch.databinding.ActivityMainBinding;
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private static final int RC_SIGN_IN = 123;
+
+    ActivityMainBinding getViewBinding() {
+        return ActivityMainBinding.inflate(getLayoutInflater());
+    }
+   private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setupListeners();
     }
+
+    private void setupListeners() {
+        // Login Button
+        binding.googleSignInButton.setOnClickListener(view -> {
+            startSignInActivity();
+        });
+    }
+        private void startSignInActivity () {
+            //Choose authentication providers
+            List<AuthUI.IdpConfig> providers =
+                    Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
+
+            // Launch the activity
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setTheme(R.style.LoginTheme)
+                            .setAvailableProviders(providers)
+                            .setIsSmartLockEnabled(false, true)
+                            .setLogo(R.drawable.go4lunch)
+                            .build(),
+                    RC_SIGN_IN);
+        }
 }
